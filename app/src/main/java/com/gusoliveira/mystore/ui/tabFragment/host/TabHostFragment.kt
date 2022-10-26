b59dab10b58483e3ae8bdd4e2b10f33b8e1e8897
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2022-10-26 01:55:36 -0300

    update 26/10/2022 1:55

--- app/src/main/java/com/gusoliveira/mystore/ui/tabFragment/host/TabHostFragment.kt
+++ app/src/main/java/com/gusoliveira/mystore/ui/tabFragment/host/TabHostFragment.kt
@@ -1,19 +1,15 @@
 package com.gusoliveira.mystore.ui.tabFragment.host
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 import com.gusoliveira.mystore.R
 import com.gusoliveira.mystore.databinding.FragmentTabHostBinding
 import com.gusoliveira.mystore.ui.tabFragment.viewmodel.TabViewModel
-import com.gusoliveira.mystore.ui.tabFragment.adapter.TabPagerAdapter
+import com.gusoliveira.mystore.ui.tabFragment.adapterHost.TabPagerAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.Timer
 
 class TabHostFragment : Fragment(R.layout.fragment_tab_host) {
 
@@ -24,13 +20,7 @@ class TabHostFragment : Fragment(R.layout.fragment_tab_host) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTabHostBinding.bind(view)
 
-
         observable()
-
-
-
-
-
     }
 
     private fun observable(){
