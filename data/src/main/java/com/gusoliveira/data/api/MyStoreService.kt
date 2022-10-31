41346b79e8b29b44a1496bae5223f592a97a198c
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2022-10-30 23:47:25 -0300

    adiciona 'TODO' ao MyStoreService

--- data/src/main/java/com/gusoliveira/data/api/MyStoreService.kt
+++ data/src/main/java/com/gusoliveira/data/api/MyStoreService.kt
@@ -5,7 +5,8 @@ import com.gusoliveira.data.model.Products
 import retrofit2.http.GET
 
 interface MyStoreService {
-    //TODO Ao invez do ItemProduct (modelo da camada Data), não posso usar ProductEntity (modelo da camada domain)?
+    //TODO Ao invez do ItemProduct (modelo da camada Data), não posso usar ProductEntity (modelo da camada domain)? devido a elas serem iguais
+    //TODO O jeito que foi declarado List<ItemProduct> a baixo ta correto? pode ser feito assim?
     @GET("products")
     suspend fun getAllProductsList(): List<ItemProduct>
 }
