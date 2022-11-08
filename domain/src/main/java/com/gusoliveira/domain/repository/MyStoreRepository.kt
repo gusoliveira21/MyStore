d051fd35cda757342b6317d2e851673fb682c450
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2022-11-08 16:06:30 -0300

    adiciona usecase que recebe uma lista de categorias

--- domain/src/main/java/com/gusoliveira/domain/repository/MyStoreRepository.kt
+++ domain/src/main/java/com/gusoliveira/domain/repository/MyStoreRepository.kt
@@ -4,4 +4,5 @@ import com.gusoliveira.domain.entities.objectProduct.ProductEntity
 
 interface MyStoreRepository {
     suspend fun getProduct():List<ProductEntity>
+    suspend fun getCategories():List<String>
 }
