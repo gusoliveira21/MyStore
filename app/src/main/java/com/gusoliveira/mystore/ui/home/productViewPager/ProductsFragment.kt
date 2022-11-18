95c127f52d372f00380f52dfff3e47eeacbb6a96
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2022-11-18 05:11:12 -0300

    refactor ProductsFragment

--- app/src/main/java/com/gusoliveira/mystore/ui/home/productViewPager/ProductsFragment.kt
+++ app/src/main/java/com/gusoliveira/mystore/ui/home/productViewPager/ProductsFragment.kt
@@ -1,7 +1,6 @@
 package com.gusoliveira.mystore.ui.home.productViewPager
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,7 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.viewpager2.widget.ViewPager2
+import com.gusoliveira.domain.entities.objectProduct.ProductEntity
 import com.gusoliveira.mystore.databinding.ViewpagerBinding
 import com.gusoliveira.mystore.ui.home.productViewPager.adapter.ItemAdapter
 import com.gusoliveira.mystore.ui.home.productViewPager.viewModel.ProductViewModel
@@ -29,37 +29,22 @@ class ProductsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewPagerConfig()
+        callGetProducts()
         observer()
     }
 
     private fun observer() {
-        viewModel.product.observe(viewLifecycleOwner, Observer { it ->
-            viewPager = binding.pagerViewP
-            viewPager?.adapter = ItemAdapter(it)
-        })
+        viewModel.product.observe(viewLifecycleOwner, Observer(::setupViewPager))
     }
 
-    private fun callFunction(position: String?) {
-        //TODO corrigir para retirar esse toString()
-        viewModel.getProducts(position.toString())
-    }
-
-    private fun viewPagerConfig() {
-        /*Log.e("log", "TabSelecionada ->${arguments?.getString(CATEGORY_NAME_BUNDLE)}")
-        position = arguments?.getString(CATEGORY_NAME_BUNDLE)
+    private fun setupViewPager(list:List<ProductEntity>){
         viewPager = binding.pagerViewP
-        position?.let {
-            when (position) {
-                "electronics" -> viewPager?.adapter = ItemAdapter(mutableListOf())
-                "jewelery" -> viewPager?.adapter = ItemAdapter(mutableListOf())
-                "men's clothing" -> viewPager?.adapter = ItemAdapter(mutableListOf())
-                "women's clothing" -> viewPager?.adapter = ItemAdapter(mutableListOf())
-            }
-        }*/
-        Log.e("log", "TabSelecionada ->${arguments?.getString(CATEGORY_NAME_BUNDLE)}")
+        viewPager.adapter = ItemAdapter(list)
+    }
+
+    private fun callGetProducts() {
         position = arguments?.getString(CATEGORY_NAME_BUNDLE)
-        callFunction(position)
+        viewModel.getProducts(position.toString())
     }
 
     companion object {
